9027e53a994cdbb8881c6988ac415e4304b38dd1
TomasSchwartz <tschwartz@frba.utn.edu.ar>
2022-05-12 21:47:25 -0300

    Update monopoly.hs

--- monopoly.hs
+++ monopoly.hs
@@ -1,8 +1,22 @@
-data Jugador = UnJugador{nombre :: String , cantidadDinero :: Int , estrategiaDeJuego :: String , propiedadesDeLasPropiedades :: [Propiedades] , acciones :: [Accion]}
+import Text.Show.Functions
+
+data Jugador = UnJugador{
+    nombre :: String , 
+    cantidadDinero :: Int , 
+    estrategiaDeJuego :: String , 
+    propiedadesDeLasPropiedades :: [Propiedades], 
+    acciones :: [Accion]
+}deriving (Show)
 type Propiedades = (String , Int)
 type Accion = Jugador -> Jugador
 
+carolina :: Jugador
+carolina = (UnJugador "Carolina" 500 "Accionista" [] [pagarAAccionistas])
+
 --maps--
+funcion :: Jugador -> Jugador
+funcion unJugador = unJugador
+
 mapNombre:: (String -> String) -> Jugador -> Jugador
 mapNombre unaFuncion unJugador = unJugador{nombre = unaFuncion.nombre $ unJugador}
 
@@ -38,12 +52,52 @@ subasta _ _ unJugador = unJugador
 cobrarAlquileres :: Jugador -> Int
 cobrarAlquileres unJugador = sum (precioDePropiedades unJugador)
 
---pagarAAccionistas :: Accion
+pagarAAccionistas :: Accion
+pagarAAccionistas (UnJugador nombre cantidadDinero "Accionista" propiedadesDeLasPropiedades acciones) = (UnJugador nombre (cantidadDinero + 200) "Accionista" propiedadesDeLasPropiedades acciones)
+pagarAAccionistas unJugador = aumentarDinero (-200) unJugador
 
-hacerBerrinchePor :: Propiedades -> Accion
-hacerBerrinchePor unaPropiedad unJugador = aumentarDinero 10 . gritar $ unJugador
+hacerBerrinchePor :: Propiedades -> Accion 
+hacerBerrinchePor unaPropiedad unJugador = (conseguirDineroDespuesDePropiedad unJugador unaPropiedad) . hacerMuchasAcciones  (take (dineroNecesarioParaUnaPropiedad unaPropiedad unJugador ) (repeat gritar)) . mapPropieadesDeCompra ((:) unaPropiedad) $ unJugador
+
+ultimaRonda :: Accion
+ultimaRonda unJugador = (devolverAcciones (acciones unJugador)) unJugador
+
+devolverAcciones ::  [Accion]  -> Accion
+devolverAcciones unaListaDeAcciones  = foldl (.) (accionNula) unaListaDeAcciones
+
+juegoFinal :: Jugador -> Jugador -> Jugador
+juegoFinal unJugador otroJugador
+    | cantidadDinero (ultimaRonda unJugador) > cantidadDinero (ultimaRonda otroJugador) = unJugador
+    | otherwise = otroJugador
 
 
+accionNula :: Accion
+accionNula unJugador = unJugador
+
+--(a -> b -> a) -> a -> [b] -> a
+--(Accion -> b -> Accion) -> Accion -> [Accion] -> Accion
+--(foldl (aplicarAccion unJugador) [] (take unaCantidad (repeat gritar)))
+
+
+conseguirDineroDespuesDePropiedad :: Jugador -> Propiedades -> Accion
+conseguirDineroDespuesDePropiedad unJugador unaPropiedad
+    | (cantidadDinero unJugador<).(snd) $ unaPropiedad = mapCantidadDinero (const 0)
+    | otherwise = mapCantidadDinero . const . ((-) (cantidadDinero unJugador)) . snd $ unaPropiedad
+
+dineroNecesarioParaUnaPropiedad :: Propiedades -> Jugador -> Int
+dineroNecesarioParaUnaPropiedad unaPropiedad unJugador = div ((snd unaPropiedad) - (cantidadDinero unJugador)) 10
+
+hacerMuchasAcciones ::  [Accion] -> Jugador -> Jugador
+hacerMuchasAcciones unaListaDeAcciones unJugador = foldl (aplicarAccion) unJugador unaListaDeAcciones
+
+--(a -> b -> a) -> a -> [b] -> a
+--(Jugador -> Accion -> Jugador) -> Jugador -> [Accion] -> Jugador
+--(foldl (aplicarAccion unJugador) [] (take unaCantidad (repeat gritar)))
+
+
+
+aplicarAccion :: Jugador -> Accion -> Jugador
+aplicarAccion unJugador unaAccion = unaAccion unJugador
 
 aumentarDinero :: Int -> Accion
 aumentarDinero unNumero unJugador = mapCantidadDinero (+unNumero) unJugador
@@ -55,7 +109,7 @@ agregarAccion :: Accion -> Accion
 agregarAccion unaAccion unJugador = mapAcciones ((:) unaAccion) unJugador
 
 agregarAlPrincipioDelNombre :: String -> Accion
-agregarAlPrincipioDelNombre unaPalabra unJugador = mapNombre (++ unaPalabra) unJugador
+agregarAlPrincipioDelNombre unaPalabra unJugador = mapNombre (unaPalabra ++) unJugador
 
 precioDePropiedades :: Jugador -> [Int]
 precioDePropiedades unJugador = map esPrecioBaratoOCaro (propiedadesDeLasPropiedades unJugador)
